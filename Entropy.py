4d6bbb33286d7b638c19479cc11373309a215043
marenwehrheim <marenwehrheim@hotmail.de>
2024-11-12 08:51:38 +0100

    updated entropy code to new and consistent data format

--- Entropy.py
+++ Entropy.py
@@ -1,103 +1,98 @@
+import os
 import argparse
 import numpy as np
 import neurokit2 as nk
 import pandas as pd
 import scipy.io
 
-from data_utils import calc_windowed_spike_matrix
+from data_utils import load_data
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
-def compute_entropy_measures_increasing_frames(average_spike_count, args):
+def compute_entropy_measures_increasing_frames(X, d_results, save_name, sample_i, args):
     # initial_frame, steps, num_shuffles, n_inits):
     """
     Compute entropy and complexity metrics on an increasing number of time frames for each neuron.
 
     Parameters:
-    - average_spike_count (np.ndarray): Averaged spike count matrix (neurons x frames).
-    - initial_frame (int): Starting number of frames for the calculation.
-    - steps (int): Number of incremental steps (i.e., compute metrics for initial_frame, initial_frame + 1, ..., initial_frame + steps).
-    - num_shuffles (int): Number of times to shuffle the input data for each neuron.
-    - n_inits (int): Number of starting conditions
+    - X (np.ndarray): data matrix (neurons x frames).
+    - d_results (dict): results dictionalry
+    - save_name (str): where to save the data (needed for intermediate saves)
+    - sample_i (int): current sample or probe to analyze
+    - args (argparse object): program arguments
+
 
     Returns:
-    - Dict: Dictionary containing entropy and complexity metrics for each neuron and frame increment.
+    - d (dict): Dictionary containing entropy and complexity metrics for each neuron and frame increment.
     """
     np.random.seed(args.seed)
-    num_neurons, max_time_frames = average_spike_count.shape
+    n_neurons, max_time_frames = X.shape
     
-    metrics_dict = {
-        'sample_entropy': np.empty((args.n_inits, num_neurons, args.steps)),
-        'sample_entropy_random': np.empty((args.num_shuffles, args.n_inits, num_neurons, args.steps)),
-        'approximate_entropy': np.empty((args.n_inits, num_neurons, args.steps)),
-        'approximate_entropy_random': np.empty((args.num_shuffles, args.n_inits, num_neurons, args.steps)),
-        'fuzzy_entropy': np.empty((args.n_inits, num_neurons, args.steps)),
-        'fuzzy_entropy_random': np.empty((args.num_shuffles, args.n_inits, num_neurons, args.steps)),
-        'weighted_permutation_entropy': np.empty((args.n_inits, num_neurons, args.steps)),
-        'weighted_permutation_entropy_random': np.empty((args.num_shuffles, args.n_inits, num_neurons, args.steps)),
-        #'hurst_exponent': np.empty((num_neurons, args.steps)),
-        #'lyapunov_exponent': np.empty((num_neurons, args.steps)),
-        'fractal_dimension_katz': np.empty((args.n_inits, num_neurons, args.steps)),
-        'fractal_dimension_katz_random': np.empty((args.num_shuffles, args.n_inits, num_neurons, args.steps)),
-        'fisher_information': np.empty((args.n_inits, num_neurons, args.steps)),
-        'fisher_information_random': np.empty((args.num_shuffles, args.n_inits, num_neurons, args.steps)),
-        'initial_frame': args.initial_frame, 
-        'steps': args.steps, 
-        'num_shuffles': args.num_shuffles, 
-        'n_inits': args.n_inits,
-        'seed': args.seed,
-        'shape': ('n_inits', 'n_neurons', 'steps'),
-        'shape_rand': ('num_shuffles', 'n_inits', 'n_neurons', 'steps')
-    }
 
-    if args.data_set == 'Stringer':
-        metrics_dict['Hz'] = 30 / args.window_size
-        save_name = f'./results/Entropy_Stringer_neuropixels_{args.animal_name}_{args.seed}.npz'
+
+    d_results[f'sample_{sample_i}'] = {
+        'sample_entropy': [np.empty((args.n_inits, n_neurons, args.steps))],
+        'sample_entropy_random': np.empty((args.num_shuffles, args.n_inits, n_neurons, args.steps)),
+        'approximate_entropy': np.empty((args.n_inits, n_neurons, args.steps)),
+        'approximate_entropy_random': np.empty((args.num_shuffles, args.n_inits, n_neurons, args.steps)),
+        'fuzzy_entropy': np.empty((args.n_inits, n_neurons, args.steps)),
+        'fuzzy_entropy_random': np.empty((args.num_shuffles, args.n_inits, n_neurons, args.steps)),
+        'weighted_permutation_entropy': np.empty((args.n_inits, n_neurons, args.steps)),
+        'weighted_permutation_entropy_random': np.empty((args.num_shuffles, args.n_inits, n_neurons, args.steps)),
+        #'hurst_exponent': np.empty((n_neurons, args.steps)),
+        #'lyapunov_exponent': np.empty((n_neurons, args.steps)),
+        'fractal_dimension_katz': np.empty((args.n_inits, n_neurons, args.steps)),
+        'fractal_dimension_katz_random': np.empty((args.num_shuffles, args.n_inits, n_neurons, args.steps)),
+        'fisher_information': np.empty((args.n_inits, n_neurons, args.steps)),
+        'fisher_information_random': np.empty((args.num_shuffles, args.n_inits, n_neurons, args.steps)),
+        }
+
     
     
 
     # Loop over initializations 
     for j in range(args.n_inits):
         print(j, end=': ', flush=True)
-        start_frame = np.random.randint(max_time_frames - (args.steps + args.initial_frame))
+        start_frame = np.random.randint(max_time_frames - (args.steps + args.min_frames))
         # Loop over each neuron
-        for i in range(num_neurons):
+        for i in range(n_neurons):
             neuron_data = average_spike_count[i, :]
             print(i, end=',', flush=True)
             # Loop over the range of increasing frames
-            for step in range(args.steps):
-                frame_count = args.initial_frame + step
+            for step in range(0, args.steps, args.step_size):
+                frame_count = args.min_frames + step
                 if frame_count > max_time_frames:
                     break
                 data_subset = neuron_data[start_frame:start_frame+frame_count]
 
                 # Calculate entropy and complexity metrics for the current frame count
                 
-                metrics_dict['sample_entropy'][j, i, step] = nk.entropy_sample(data_subset)[0]
-                metrics_dict['approximate_entropy'][j, i, step] = nk.entropy_approximate(data_subset)[0]
-                metrics_dict['fuzzy_entropy'][j, i, step] = nk.entropy_fuzzy(data_subset)[0]
-                metrics_dict['weighted_permutation_entropy'][j, i, step] = nk.entropy_permutation(data_subset, weighted=True)[0]
-                # metrics_dict['hurst_exponent'][j, i, step] = nk.fractal_hurst(data_subset)
-                # metrics_dict['lyapunov_exponent'][j, i, step] = nk.entropy_lyapunov(data_subset)
-                metrics_dict['fractal_dimension_katz'][j, i, step] = nk.fractal_katz(data_subset)[0]
-                metrics_dict['fisher_information'][j, i, step] = nk.fisher_information(data_subset)[0]
+                d_results[f'sample_{sample_i}']['sample_entropy'][j, i, step] = nk.entropy_sample(data_subset)[0]
+                d_results[f'sample_{sample_i}']['approximate_entropy'][j, i, step] = nk.entropy_approximate(data_subset)[0]
+                d_results[f'sample_{sample_i}']['fuzzy_entropy'][j, i, step] = nk.entropy_fuzzy(data_subset)[0]
+                d_results[f'sample_{sample_i}']['weighted_permutation_entropy'][j, i, step] = nk.entropy_permutation(data_subset, weighted=True)[0]
+                # d_results[f'sample_{sample_i}']['hurst_exponent'][j, i, step] = nk.fractal_hurst(data_subset)
+                # d_results[f'sample_{sample_i}']['lyapunov_exponent'][j, i, step] = nk.entropy_lyapunov(data_subset)
+                d_results[f'sample_{sample_i}']['fractal_dimension_katz'][j, i, step] = nk.fractal_katz(data_subset)[0]
+                d_results[f'sample_{sample_i}']['fisher_information'][j, i, step] = nk.fisher_information(data_subset)[0]
                 
                 for shuffle_i in range(args.num_shuffles):
                     shuffled_data = np.random.permutation(data_subset)
-                    metrics_dict['sample_entropy_random'][shuffle_i, j, i, step] = nk.entropy_sample(shuffled_data)[0]
-                    metrics_dict['approximate_entropy_random'][shuffle_i, j, i, step] = nk.entropy_approximate(shuffled_data)[0]
-                    metrics_dict['fuzzy_entropy_random'][shuffle_i, j, i, step] = nk.entropy_fuzzy(shuffled_data)[0]
-                    metrics_dict['weighted_permutation_entropy_random'][shuffle_i, j, i, step] = nk.entropy_permutation(shuffled_data, weighted=True)[0]
-                    # metrics_dict['hurst_exponent'][shuffle_i, j, i, step] = nk.fractal_hurst(shuffled_data)
-                    # metrics_dict['lyapunov_exponent'][shuffle_i, j, i, step] = nk.entropy_lyapunov(shuffled_data)
-                    metrics_dict['fractal_dimension_katz_random'][shuffle_i, j, i, step] = nk.fractal_katz(shuffled_data)[0]
-                    metrics_dict['fisher_information_random'][shuffle_i, j, i, step] = nk.fisher_information(shuffled_data)[0]
+                    d_results[f'sample_{sample_i}']['sample_entropy_random'][shuffle_i, j, i, step] = nk.entropy_sample(shuffled_data)[0]
+                    d_results[f'sample_{sample_i}']['approximate_entropy_random'][shuffle_i, j, i, step] = nk.entropy_approximate(shuffled_data)[0]
+                    d_results[f'sample_{sample_i}']['fuzzy_entropy_random'][shuffle_i, j, i, step] = nk.entropy_fuzzy(shuffled_data)[0]
+                    d_results[f'sample_{sample_i}']['weighted_permutation_entropy_random'][shuffle_i, j, i, step] = nk.entropy_permutation(shuffled_data, weighted=True)[0]
+                    # d_results[f'sample_{sample_i}']['hurst_exponent'][shuffle_i, j, i, step] = nk.fractal_hurst(shuffled_data)
+                    # d_results[f'sample_{sample_i}']['lyapunov_exponent'][shuffle_i, j, i, step] = nk.entropy_lyapunov(shuffled_data)
+                    d_results[f'sample_{sample_i}']['fractal_dimension_katz_random'][shuffle_i, j, i, step] = nk.fractal_katz(shuffled_data)[0]
+                    d_results[f'sample_{sample_i}']['fisher_information_random'][shuffle_i, j, i, step] = nk.fisher_information(shuffled_data)[0]
         print('')  
-        np.savez_compressed(save_name, **metrics_dict)      
+        np.savez_compressed(save_name, **d_results)      
 
-    return metrics_dict
+    return d_results
+
 
 if __name__ == '__main__':
 
@@ -106,28 +101,35 @@ if __name__ == '__main__':
                     description='What the program does',
                     epilog='Text at the bottom of help')
     parser.add_argument('-d', '--data_set', type=str, default='Stringer', help='Name of the data set to use')
-    parser.add_argument('--initial_frame', type=int, default=5, help='Minimum number of frames to use')
+    parser.add_argument('--min_frames', type=int, default=5, help='Minimum number of frames to use')
     parser.add_argument('--steps', type=int, default=400, help='Number of time frames to compute the entropy for')
+    parser.add_argument('--step_size', type=int, default=1, help='step size to increase the number of frames')
     parser.add_argument('--num_shuffles', type=int, default=100, help='Number of random shuffles')
     parser.add_argument('--n_inits', type=int, default=20, help='Number of starting conditions')
     parser.add_argument('--window_size', type=int, default=10, help='Non-overlapping sliding window width')
     parser.add_argument('--animal_name', type=str, default='Krebs', help='Name of animal to load')
     parser.add_argument('--seed', type=int, default=0, help='initialization seed for random selection of start frames')
+    parser.add_argument('--data_dir', type=str, default='./', help='directory to the data (without the data folder)')
     
     args = parser.parse_args()
     print(args)
+    X, d, save_name = load_data(args, 'Entropy')
 
-    if args.data_set == 'Stringer':
-        
-        f = scipy.io.loadmat(f'./data/StringerNeuropixels/{args.animal_name}withFaces_KS2.mat')
-        data_matrix = calc_windowed_spike_matrix(f['stall'], args.window_size)
-        area_labels = [x[0] for x in f['areaLabels'][0]]
-        print('data_matrix.shape', data_matrix.shape)
+    d['meta_data'].update({'min_frames': args.min_frames, 
+                            'steps': args.steps, 
+                            'num_shuffles': args.num_shuffles, 
+                            'n_inits': args.n_inits,
+                            'seed': args.seed,
+                            'shape': ('n_inits', 'n_neurons', 'steps'),
+                            'shape_rand': ('num_shuffles', 'n_inits', 'n_neurons', 'steps')})
 
-    entropy_results = compute_entropy_measures_increasing_frames(data_matrix, args)
-    #args.initial_frame, args.steps, 
-    #                                                            args.num_suffles, args.n_inits, Hz)
+    
+    for sample_i, x in enumerate(X):
+        if i == 0:
+            print('x.shape', x.shape)
+        if i % 100 == 0:
+            print(i, end=',')
 
-    if args.data_set == 'Stringer':
-        # entropy_results['Hz'] = 30 / args.window_size
-        np.savez_compressed(f'./results/Entropy_Stringer_neuropixels_{args.animal_name}_{args.seed}.npz', **entropy_results)
+        d = compute_entropy_measures_increasing_frames(x, d, save_name, sample_i, args)
+
+    np.savez_compressed(save_name, **d)
