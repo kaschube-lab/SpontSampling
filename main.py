9dcb8a1f16fe3bf4caf51bdf5b009edea846d58d
Maren Wehrheim <wehrheim@login02.cm.cluster>
2024-12-10 15:52:47 +0100

    main updated

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from data_utils import load_data
 from Curvature import calc_curvature_full
 from Dimensionality import calc_dimensionality_increasing_frames
 from Entropy import calc_entropy_measures_increasing_frames
-from kNN import calc_kNN
+# from kNN import calc_kNN
+from CosineDistances import calc_avrg_knn
 
 import numpy as np
 import pickle
@@ -11,44 +12,43 @@ import pickle
 def main(args):
     X, area_labels, locations = load_data(args)
     d_results = init_results_dict(len(X), area_labels, locations, args)
-    functions = args.functions.split(';')
+
     save_path = get_save_path(args)
-    for function in functions:
-        np.random.seed(args.seed)
-        update_res_dict(d_results, X, function, args)
-        for i, x in enumerate(X):
-            n_frames_dt = x.shape[-1] // args.dt
-            x = x[:, :int(n_frames_dt*args.dt)]
+    np.random.seed(args.seed)
+    update_res_dict(d_results, X, args.function, args)
+    for i, x in enumerate(X):
+        n_frames_dt = x.shape[-1] // args.dt
+        x = x[:, :int(n_frames_dt*args.dt)]
 
-            if i == 0:
-                print('x.shape', x.shape, end=': ')
-            if i % 100 == 0:
-                print(i, end=',')
+        if i == 0:
+            print('x.shape', x.shape, end=': ')
+        if i % 100 == 0:
+            print(i, end=',')
 
-            if function.lower() == 'curvature':
-                calc_curvature_full(x, d_results[f'sample_{i}'], args)
-            elif function.lower() == 'nn':
-                for j in range(args.dt):
-                    calc_kNN(x, d_results[f'sample_{i}'], j, args)
-            elif function.lower() in ['entropy', 'dimensionality']:
-                for j in range(args.n_inits):
-                    print(j, end=': ', flush=True)
-                    if function.lower() == 'entropy':
-                        calc_entropy_measures_increasing_frames(x, d_results[f'sample_{i}'], j, args)
-                    elif function.lower() == 'dimensionality':
-                        calc_dimensionality_increasing_frames(x, d_results[f'sample_{i}'], j, args)
-                    # Intermediate save, as the computation takes long, also in case of a program failure 
-                    if j % 5 == 0:
-                        np.savez_compressed(save_path, **d_results)
-            
-            # Save after each trial/sample/probe
-            try:
-                np.savez_compressed(save_path, **d_results)
-            except OverflowError:
-                save_path = save_path.split('.npz')[0] + 'pkl'
-                with open(save_path, 'wb') as f:
-   ...:             pickle.dump(d_results, f, protocol=4)
-            print('')
+        if args.function.lower() == 'curvature':
+            calc_curvature_full(x, d_results[f'sample_{i}'], args)
+        elif args.function.lower() == 'knn':
+            for j in range(args.dt):
+                calc_avrg_knn(x, d_results[f'sample_{i}'], j, args)
+        elif args.function.lower() in ['entropy', 'dimensionality']:
+            for j in range(args.n_inits):
+                print(j, end=': ', flush=True)
+                if args.function.lower() == 'entropy':
+                    calc_entropy_measures_increasing_frames(x, d_results[f'sample_{i}'], j, args)
+                elif args.function.lower() == 'dimensionality':
+                    calc_dimensionality_increasing_frames(x, d_results[f'sample_{i}'], j, args)
+                # Intermediate save, as the computation takes long, also in case of a program failure 
+                if j % 5 == 0:
+                    np.savez_compressed(save_path, **d_results)
+        
+        # Save after each trial/sample/probe
+        try:
+            np.savez_compressed(save_path, **d_results)
+        except OverflowError:
+            save_path = save_path.split('.npz')[0] + 'pkl'
+            with open(save_path, 'wb') as f:
+...:             pickle.dump(d_results, f, protocol=4)
+        print('')
 
 if __name__ == '__main__':
 
