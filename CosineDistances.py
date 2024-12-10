1198750c0a45f20fb0e4153535e4fcbafa76a32e
Maren Wehrheim <wehrheim@login02.cm.cluster>
2024-12-10 17:11:15 +0100

    added cosine dist computation in beginning

--- CosineDistances.py
+++ CosineDistances.py
@@ -3,41 +3,27 @@ from heapq import nsmallest
 from scipy.spatial.distance import cosine
 from control_data import create_gauss
 
-
-def compute_min_cosine_distances(data, window_size):
+def compute_cosine_dist_matrix(x, args):
     """
-    Compute the minimal cosine distance of each pattern to preceding and following patterns within a timeframe.
-
-    Args:
-        data (np.ndarray): Array of shape (n_neurons, n_timeframes).
-        window_size (int): The number of time frames to consider before and after.
-
+    Computes the cosine distance matrix for data and the number of time frames selected.
+    Args: 
+        x (np.ndarray): Data array of shape (n_pixels, n_timeframes).
+        args (argparse object): program arguments
     Returns:
-        tuple: Two arrays of shape (n_timeframes,) containing the minimal cosine distances to 
-               preceding and following patterns for each timepoint.
+        cosine_dist (np.ndarray): upper triangular matrix with cosine distances.
     """
-    n_neurons, n_timeframes  = data.shape
-    
-    # Initialize arrays to store minimal distances
-    min_dist_to_preceding = np.full(n_timeframes, np.inf)
-    min_dist_to_following = np.full(n_timeframes, np.inf)
-    
+    _, n_timeframes = x.shape
+    cosine_dist = np.empty((n_timeframes, n_timeframes))
     for t in range(n_timeframes):
-        # Compute distances to preceding patterns with the current timeframe
-        for tf in range(max(0, t - window_size), t):
-            distance = cosine(data[:, t], data[:, tf])
-            min_dist_to_preceding[t] = min(min_dist_to_preceding[t], distance)
-        
-        # Compute distances to following patterns with the current timeframe
-        for tf in range(t + 1, min(n_timeframes, t + window_size + 1)):
-            distance = cosine(data[:, t], data[:, tf])
-            min_dist_to_following[t] = min(min_dist_to_following[t], distance)
-    
-    return min_dist_to_preceding, min_dist_to_following
+        for offset in range(args.window_size):
+            try:
+                cosine_dist[t, t+offset] = cosine(x[:, t], x[:, t+offset])
+            except:
+                continue
+    return cosine_dist
 
 
-
-def compute_avg_min_cosine_distances(x, t, avg_min_dist_to_preceding, avg_min_dist_to_following, args):
+def compute_avg_min_cosine_distances(x, avg_min_dist_to_preceding, avg_min_dist_to_following, args):
     """
     Compute the average minimal cosine distance to the nearest k patterns 
     for each pattern to preceding and following patterns within a timeframe.
@@ -54,28 +40,21 @@ def compute_avg_min_cosine_distances(x, t, avg_min_dist_to_preceding, avg_min_di
 
     """
     _, n_timeframes = x.shape
-    if t >= args.window_size:
-        preceding_distances = []
-        for tf in range(max(0, t - args.window_size), t):
-            distance = cosine(x[:, t], x[:, tf])
-            preceding_distances.append(distance)
-        
-        # Take the average of the smallest k distances
-        if preceding_distances:
+    cosine_dist = compute_cosine_dist_matrix(x, args)
+    for t in range(n_timeframes):
+        if t >= args.window_size:
+            preceding_distances = cosine_dist[t-args.window_size:t, t]
+            # Get the k smallest distances
             k_smallest_preceding = nsmallest(args.k, preceding_distances)
+            # Take the average of the smallest k distances
             avg_min_dist_to_preceding[t - args.window_size] = np.mean(k_smallest_preceding)
- 
-    if t <= n_timeframes - args.window_size - 1:
-        # Compute distances to following patterns within the timeframe
-        following_distances = []
-        for tf in range(t + 1, min(n_timeframes, t + args.window_size + 1)):
-            distance = cosine(x[:, t], x[:, tf])
-            following_distances.append(distance)
-        
-        # Take the average of the smallest k distances
-        if following_distances:
+
+        if t <= n_timeframes - args.window_size - 1:
+            # Compute distances to following patterns within the timeframe
+            following_distances = cosine_dist[t, t+1: t+args.window_size+1]
+            # Get the k smallest distances
             k_smallest_following = nsmallest(args.k, following_distances)
-            avg_min_dist_to_following
+            # Take the average of the smallest k distances
             avg_min_dist_to_following[t] = np.mean(k_smallest_following)
 
 
@@ -114,18 +93,16 @@ def calc_avrg_knn(x, d_results_sample, j, args):
 
     x_gauss = create_gauss(x)
 
-
-    for t in range(n_timeframes):
-        # Compute distances to preceding patterns within the timeframe
-        compute_avg_min_cosine_distances(x, t, d_results_sample['avg_min_dist_to_preceding'][j], 
+    print('compute knn for normal data', flush=True)
+    compute_avg_min_cosine_distances(x, d_results_sample['avg_min_dist_to_preceding'][j], 
                                         d_results_sample['avg_min_dist_to_following'][j], args)
-        
-        compute_avg_min_cosine_distances(x_gauss, t, d_results_sample['avg_min_dist_to_preceding_gauss'][j], 
+
+    print('compute knn for Gauss', flush=True)
+    compute_avg_min_cosine_distances(x_gauss, d_results_sample['avg_min_dist_to_preceding_gauss'][j], 
                                          d_results_sample['avg_min_dist_to_following_gauss'][j], args)
 
-
-        for shuffle_i in range(args.n_shuffles):
-            X_shuffled = np.apply_along_axis(np.random.permutation, 1, x)
-            compute_avg_min_cosine_distances(X_shuffled, t, 
-                                            d_results_sample['avg_min_dist_to_preceding_random'][shuffle_i, j], 
-                                            d_results_sample['avg_min_dist_to_following_random'][shuffle_i, j], args)
+    print('compute knn for shuffled data', flush=True)
+    for shuffle_i in range(args.n_shuffles):
+        X_shuffled = np.apply_along_axis(np.random.permutation, 1, x)
+        compute_avg_min_cosine_distances(X_shuffled, d_results_sample['avg_min_dist_to_preceding_random'][shuffle_i, j], 
+                                        d_results_sample['avg_min_dist_to_following_random'][shuffle_i, j], args)
