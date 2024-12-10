08d8ceeca9239c506210c879f6a25a6bf539a487
Maren Wehrheim <wehrheim@login02.cm.cluster>
2024-12-10 15:59:49 +0100

    adjusted control gauss to create control data in same format as input data

--- control_data.py
+++ control_data.py
@@ -4,17 +4,16 @@ from scipy.stats import multivariate_normal
 
 # Create control data
 
-def create_gauss(X, n_samples):
+def create_gauss(X):
     """
     Method to create control data sampled from a gaussian distribution based on covariance of data
     Parametres:
-        X (np.array): data array with shape (number_pixels, number_time_frames)
-        n_samples (int): number of synthetic data points
+        X (np.array): data array with shape (n_pixels, n_time_frames)
 
     Returns:
-    - Y (np.array): synthetically created control data
+    - Y (np.array): synthetically created control data in the same shape as X
     """
-
+    _, n_timeframes = X.shape
     # Compute the mean vector (average over timepoints for each variable)
     mean = np.mean(X, axis=1)
     # Compute the covariance matrix (pairwise covariance between variables)
@@ -23,12 +22,12 @@ def create_gauss(X, n_samples):
     # Create a multivariate normal distribution object
     mvn = multivariate_normal(mean=mean, cov=covariance, allow_singular=True)
 
-    Y = mvn.rvs(size=n_samples).T  # Transpose to match (number_pixels, n_samples)
-
-    return Y
+    Y = mvn.rvs(size=n_timeframes).T  # Transpose to match (n_pixels, n_timeframes)
+    return [Y]
 
 
 def load_movie(data_dir='./', movie=1):
+    # Potential ToDo: Load all movies into the list
     if movie == 1:
         folder_path = os.path.join(data_dir, "AllenInstitute_Movies/natural_movie_one.npy")
     elif movie == 3:
@@ -37,4 +36,4 @@ def load_movie(data_dir='./', movie=1):
     movie_data = np.load(folder_path)
     movie_flat = movie_data.reshape(movie_data.shape[0],-1).T  # reshape to fit (n_pixels, n_timeframes)
 
-    return movie_flat
+    return [movie_flat]
